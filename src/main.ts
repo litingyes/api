13e04045488be71118fe651fe9f99852094d53ca
liting <luz.liting@gmail.com>
2023-01-29 00:51:33 +0800

    feat: add express-rate-limit

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,16 @@
 import { NestFactory } from '@nestjs/core';
+import rateLimit from 'express-rate-limit';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+  app.use(
+    rateLimit({
+      windowMs: 15 * 60 * 1000,
+      max: 100,
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
