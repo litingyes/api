9c866960dd83809c3d6eecea3287a009ec679e37
liting <luz.liting@gmail.com>
2023-01-31 01:59:50 +0800

    feat: add helmet

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import rateLimit from 'express-rate-limit';
+import helmet from 'helmet';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+  app.use(helmet());
   app.use(
     rateLimit({
       windowMs: 15 * 60 * 1000,
